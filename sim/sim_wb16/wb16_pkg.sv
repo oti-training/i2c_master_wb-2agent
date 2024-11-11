c584cb78ef18f65c0ec5f9e26845e354788c7be3
unknown <michmanullang@gmail.com>
2024-11-11 21:06:23 +0700

    Updated WBS16 (ngikutin format WBS8)

---
+++ sim/sim_wb16/wb16_pkg.sv
@@ -0,0 +1,20 @@
+package wb16_pkg;
+    import uvm_pkg::*;
+
+    `include "uvm_macros.svh"
+
+    // compile order
+    `include "sequence_item.sv"
+    `include "config.sv"
+    `include "sequence.sv"
+    `include "sequence_slave.sv"
+    `include "monitor.sv"
+    `include "driver.sv"
+    `include "driver_slave.sv"
+    `include "agent.sv"
+    `include "agent_slave.sv"
+    `include "scoreboard.sv"
+    `include "coverage.sv"
+    `include "environment.sv"
+    `include "test.sv"
+endpackage
